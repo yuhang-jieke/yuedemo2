bf203dbc4a083ec623f3d03218d0f54165736872
biyuhang <3530507377@qq.com>
2026-02-02 10:24:00 +0800

    fix

--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func main() {
-	//pkg.GetTitle("https://sale.1688.com/factory/u0vjcc4j.html?spm=a260k.home2025.centralDoor.ddoor.66333597BBbHgE&topOfferIds=1005591171200", ".offerTitle")
-	//pkg.GetImg("https://sale.1688.com/factory/u0vjcc4j.html?spm=a260k.home2025.centralDoor.ddoor.66333597BBbHgE&topOfferIds=1005591171200", ".offerImg")
-	//pkg.GetPrice()
-}
